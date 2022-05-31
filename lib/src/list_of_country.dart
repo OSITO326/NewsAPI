eb92ad555933d065130849aefe829aaac3897a89
Kevin <326.osito@gmail.com>
2022-05-31 16:48:22 -0400

    first commit

---
+++ lib/src/list_of_country.dart
@@ -0,0 +1,34 @@
+const listOfCountry = [
+  {"name": "Argentina", "code": "ar"},
+  {"name": "Colombia", "code": "ar"},
+  {"name": "México", "code": "mx"},
+  {"name": "United States", "code": "us"},
+  {"name": "Venezuela", "code": "ve"},
+  /*{"name": "United Arab Emirates", "code": "ae"},*/
+  /*{"name": "New Zealand", "code": "nz"},*/
+  /*{"name": "Israel", "code": "il"},*/
+  /*{"name": "Indonesia", "code": "id"},*/
+  /*{"name": "INDIA", "code": "in"},*/
+];
+
+const listOfCategory = [
+  {"name": "Ciencia", "code": "science"},
+  {"name": "Negocios", "code": "business"},
+  {"name": "Tecnología", "code": "technology"},
+  {"name": "Deporte", "code": "sports"},
+  {"name": "Salud", "code": "health"},
+  {"name": "General", "code": "general"},
+  {"name": "Entretenimiento", "code": "entertainment"},
+  {"name": "Todos", "code": null},
+];
+const listOfNewsChannel = [
+  {"name": "BBC News", "code": "bbc-news"},
+  /*{"name": "The Times of India", "code": "the-times-of-india"},*/
+  /*{"code": "politico", "name": "politico"},*/
+  {"code": "the-washington-post", "name": "The Washington Post"},
+  /*{"code": "reuters", "name": "reuters"},*/
+  {"code": "cnn", "name": "cnn"},
+  {"code": "nbc-news", "name": "nbc news"},
+  {"code": "the-hill", "name": "The Hill"},
+  {"code": "fox-news", "name": "Fox News"},
+];
